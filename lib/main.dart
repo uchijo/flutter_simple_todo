c7619dcfb130c6a0e8233079aee2df49fec4d9e5
Uchijo <kgtnttki.yuta@gmail.com>
2024-04-09 19:04:40 +0900

    todo追加可能

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,86 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: TodoList(),
       ),
     );
   }
+}
+
+class TodoList extends StatefulWidget {
+  const TodoList({super.key});
+
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  final _todos = <Todo>[];
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView(
+            children: [
+              ..._todos.map(
+                (e) => ListTile(
+                  title: Text(e.title),
+                  subtitle: Text(e.description),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                ),
+                controller: _titleController,
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                ),
+                controller: _descriptionController,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _todos.add(
+                    Todo(
+                      title: _titleController.text,
+                      description: _descriptionController.text,
+                    ),
+                  );
+                });
+                _titleController.clear();
+                _descriptionController.clear();
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class Todo {
+  final String title;
+  final String description;
+
+  Todo({
+    required this.title,
+    required this.description,
+  });
 }
